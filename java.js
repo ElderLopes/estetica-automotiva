97fe17c1fac3df0d53369715a05e412871bd3652
ElderLopes <elderfl85@hotmail.com>
2023-04-21 10:07:34 -0300

    pagina estetica automotiva

---
+++ java.js
@@ -0,0 +1,30 @@
+document.cookie = "cookieName=cookieValue; SameSite=None; Secure";
+
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {slideIndex = 1}
+
+  slides[slideIndex-1].style.display = "block";  
+
+  setTimeout(showSlides, 2000); // Muda a imagem a cada 2 segundos
+}
